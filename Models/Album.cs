d50bba8a2ee49012be41eeac55538947497133da
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2017-05-01 18:27:23 +0200

    First models addition

---
+++ Models/Album.cs
@@ -0,0 +1,12 @@
+namespace mvcmusicstore.Models
+{
+    public class Album
+    {
+        public int AlbumID { get; set; }
+
+        public string Title { get; set; }
+
+        public Artist Artist { get; set; }
+        
+    }
+}
